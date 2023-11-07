2decc8d334f16a84db1c415b2b4dc61505aed24a
nanduthota <147487793+nanduthota@users.noreply.github.com>
2023-11-07 21:39:32 +0530

    #include<stdio.h> #include<math.h> int main() {     float ci,p,r,t;     scanf("%f%f%f",&p,&r,&t);     ci=(p*pow((1+r/100),t)-p);     printf("%.2f",ci); }

--- Compound_Interest.c
+++ Compound_Interest.c
@@ -2,9 +2,8 @@
 #include<math.h>
 int main()
 {
-    float ci,p,t,r;
-    scanf("%f %f %f",&p,&r,&t);
+    float ci,p,r,t;
+    scanf("%f%f%f",&p,&r,&t);
     ci=(p*pow((1+r/100),t)-p);
-    
     printf("%.2f",ci);
 }
