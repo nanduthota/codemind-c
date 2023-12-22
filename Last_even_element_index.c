7de1d1700efa40a069c7261f9028a04598f8de20
nanduthota <147487793+nanduthota@users.noreply.github.com>
2023-12-22 21:41:54 +0530

    #include <stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=0;i--)     {         if(arr[i]%2==0)         {             printf("%d",i);             break;
     }
    }
    }

---
+++ Last_even_element_index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("%d",i);
+            break;
+
+ }
+}
+}
